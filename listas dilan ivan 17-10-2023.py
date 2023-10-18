89279cd6d555ca7335117feec1910ee5567d389e
ivan-malaver <malaver6@gmail.com>
2023-10-17 20:36:37 -0500

    Add files via upload

---
+++ listas dilan ivan 17-10-2023.py
@@ -0,0 +1,8 @@
+lista=  ["lunes","martes","miercoles","jueves","viernes","sabado"]
+print (lista [4])
+lista=  ["lunes","martes","miercoles","jueves","viernes","sabado"]
+print (lista )
+lista=  ["lunes","martes","miercoles","jueves","viernes","sabado"]
+print (lista [0:3])
+lista=  ["lunes","martes","miercoles","jueves","viernes","sabado",1,2,3["manuel",0.2,0.25,True]]
+print (lista [0:3])
