b68bc5acf519d8ec0bd201017bf6ff4369669569
Dilan <65737982+dailerbee@users.noreply.github.com>
2023-10-18 20:46:07 -0500

    Add files via upload

---
+++ hola mundo.py
@@ -0,0 +1,7 @@
+# primer programa que escribes en un nuevo lenguaje
+print("***********************************************************************\n\n\n")
+print("hola mundo , hello world dilan ")
+print("\n\n\n         El texto e pamtalla sirve de interacion con el usuariode un programa \n         Es precindible, sin embargo una adecuada interfasz permite claridad en la \n         ejecucion\n\n\n")
+print("***********************************************************************\n\n\n")
+print ("autores:\n\n        Dilan Camilo Martinez Rico\n        Ivan Malaver Fierro") 
+print("\n\n***********************************************************************\n\n\n")   
