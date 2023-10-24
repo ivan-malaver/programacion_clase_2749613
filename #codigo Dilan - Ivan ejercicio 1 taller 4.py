ca83a683885883a498c1270c1a4be9030154686e
Dilan <65737982+dailerbee@users.noreply.github.com>
2023-10-24 08:11:31 -0500

    Add files via upload

---
+++ #codigo Dilan - Ivan ejercicio 1 taller 4.py
@@ -0,0 +1,18 @@
+#codigo Dilan - Ivan
+A = float (input ("ingrese su primer numero a comparar "))
+B = float (input ("ingrese su segundo numero a comparar "))
+C = float (input ("ingrese su tercer numero a comparar "))
+print ("su primer numero es:", A, "segundo numero", B, "tercer numero", C)
+M= float 
+if A>B:
+    if A>C:
+        M=A
+    else:
+        M=C
+else:
+    if B>C:
+        M=B
+    else:
+        M=C
+        
+print("el numero mayore es:",M)
