8ccfca8e288c4d870489b68a0b7b07c5377e1955
ivan-malaver <malaver6@gmail.com>
2023-10-30 18:48:07 -0500

    Add files via upload

---
+++ parámetro del ciclo while.py
@@ -0,0 +1,10 @@
+N = int (input("Escriba un numero :"))
+aux1 = int (input("parámetro del ciclo while :"))
+print ("los numeros escritos son ", N, aux1)
+
+N = 1
+while N < aux1:
+    valor= aux1**N
+    
+    print (N)
+    print ("fin")
