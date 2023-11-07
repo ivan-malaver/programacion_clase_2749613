3b97cba385ce3483438250f8b22f8a866015ca21
Dilan <65737982+dailerbee@users.noreply.github.com>
2023-11-07 09:22:35 -0500

    Add files via upload

---
+++ 08_menu_seleccion dilan-ivan.py
@@ -0,0 +1,40 @@
+print ("esciba el numero de laods que tiene el pligono regular")
+lads=int(input())
+print("escriba el valores")
+a=int(input())
+b=int(input())
+match lads:
+    case 1:
+        print("error no hay pligonos de un lado")
+    case 2:
+        print("Error no hay pologonos de dos lados ") 
+    case 3:
+        area=a*b/2
+        print(area)
+    case 4:
+        area=a*b
+        print(area)
+    case 5:
+        area=a*b/2
+        print(area)
+    case default:
+        print("error el programa solo puede manejar hasta 5 lados")
+
+
+    
+  
+    
+
+         
+   
+    
+
+     
+        
+        
+    
+
+
+
+ 
+        
